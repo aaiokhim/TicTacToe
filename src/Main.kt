3b0721db2c81ff77cb1c63bf828dcd624470716a
iokhim.aa <iokhim.aa@dozen.mephi.ru>
2025-10-24 18:07:08 +0300

    Show the field

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Enter condition of the field:")
+    print("> ")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val field = readln()
+    val lowerLine = "-"
+    val count = 9
+    println(lowerLine.repeat(count))
+    println("| ${field[0]} ${field[1]} ${field[2]} |")
+    println("| ${field[3]} ${field[4]} ${field[5]} |")
+    println("| ${field[6]} ${field[7]} ${field[8]} |")
+    println(lowerLine.repeat(count))
 }
