d9fa45fd7e074c62766899dfb9c081af4f62f78d
iokhim.aa <iokhim.aa@dozen.mephi.ru>
2025-10-24 21:34:53 +0300

    working game without checks

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,75 @@
 fun main() {
-    println("Enter condition of the field:")
-    print("> ")
-
-    val field = readln()
-    if (field.length != 9 ) return
-
+    val field = MutableList(9) { '_' }
     showField(field)
 
+    var state: Boolean = true
+    var count = 0
+
+    while (state) {
+        if (count % 2 == 0) {
+            println("Player X, please make a move")
+            move(field, count)
+        } else {
+            println("Player O, please make a move")
+            move(field, count)
+        }
+
+        showField(field)
+        state = stateGame(field)
+        count += 1
+    }
+}
+
+fun move(field : MutableList<Char>, count : Int) {
+    println("Please enter coordinates separated by spaces.")
+    print("> ")
+
+    val coordinates = readln().split(" ")
+    val x = coordinates[0].toIntOrNull()
+    val y = coordinates[1].toIntOrNull()
+
+    if (x == null || y == null) return
+
+    val newCoordinate = (x - 1) * 3 + (y - 1)
+
+    if (field[newCoordinate] != '_') {
+        return
+    } else  if (count % 2 == 0){
+        field[newCoordinate] = 'X'
+    } else {
+        field[newCoordinate] = 'O'
+    }
+}
+
+fun stateGame(field : MutableList<Char>) : Boolean {
     val xWin = checkFieldState(field, 'X')
     val oWin = checkFieldState(field, 'O')
 
     when {
-        xWin && oWin -> println("Unreal situation")
-        xWin -> println("X wins")
-        oWin -> println("O wins")
-        field.contains('_') -> println("The game continues")
-        else -> println("Draw")
+        xWin && oWin -> {
+            println("Unreal situation")
+            return false
+        }
+        xWin -> {
+            println("X wins")
+            return false
+        }
+        oWin -> {
+            println("O wins")
+            return false
+        }
+        field.contains('_') -> {
+            println("The game continues")
+            return true
+        }
+        else -> {
+            println("Draw")
+            return false
+        }
     }
-
 }
 
-fun checkFieldState(field : String, player : Char) : Boolean {
+fun checkFieldState(field : MutableList<Char>, player : Char) : Boolean {
     for (i in 0..2) {
         if (field[i * 3] == player && field[i * 3 + 1] == player && field[i * 3 + 2] == player) {
             return true
@@ -40,7 +89,7 @@ fun checkFieldState(field : String, player : Char) : Boolean {
     return false
 }
 
-fun showField(field : String) {
+fun showField(field : MutableList<Char>) {
     val lowerLine = "-"
     val count = 9
     println(lowerLine.repeat(count))
