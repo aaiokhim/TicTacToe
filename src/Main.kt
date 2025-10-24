348c7985fa1bef78491a321b7d36de13b6a50461
iokhim.aa <iokhim.aa@dozen.mephi.ru>
2025-10-24 19:34:00 +0300

    fun check field state

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,44 @@ fun main() {
     print("> ")
 
     val field = readln()
+    if (field.length != 9 ) return
+
+    showField(field)
+
+    val xWin = checkFieldState(field, 'X')
+    val oWin = checkFieldState(field, 'O')
+
+    when {
+        xWin && oWin -> println("Unreal situation")
+        xWin -> println("X wins")
+        oWin -> println("O wins")
+        field.contains('_') -> println("The game continues")
+        else -> println("Draw")
+    }
+
+}
+
+fun checkFieldState(field : String, player : Char) : Boolean {
+    for (i in 0..2) {
+        if (field[i * 3] == player && field[i * 3 + 1] == player && field[i * 3 + 2] == player) {
+            return true
+        } else if (field[i] == player && field[i + 3] == player && field[i + 6] == player) {
+            return true
+        }
+    }
+
+    if (field[0] == player && field[4] == player && field[8] == player) {
+        return true
+    }
+
+    if (field[2] == player && field[4] == player && field[6] == player) {
+        return true
+    }
+
+    return false
+}
+
+fun showField(field : String) {
     val lowerLine = "-"
     val count = 9
     println(lowerLine.repeat(count))
